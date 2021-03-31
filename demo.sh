37d918f61ccf729e8b71adc7a155f3a66d4c052e
cheeun <cheeun0914@gmail.com>
2021-03-31 17:00:25 +0900

    add rrdb

---
+++ demo.sh
@@ -0,0 +1,9 @@
+python demo.py \
+--input test1_318x540.png \
+--model pretrained_models/edsr-baseline-liif.pth \
+--resolution 632,1080 \
+--output output1.png \
+--gpu 0 \
+# convert the test image to LIIF and present it in a given resolution
+# change H, W
+
